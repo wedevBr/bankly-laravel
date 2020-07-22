e3e323758a27564a25aa52ea443fb35c0af25754
Adeildo Júnior <adeildo@capitaldigitalaberto.com.br>
2020-07-22 11:44:44 -0300

    [Feature] Added tests for BankAccountValidation

---
+++ tests/BankAccountTest.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * BankAccountTest class
+ *
+ * PHP version 7.3
+ *
+ * @author    WeDev Brasil Team <contato@wedev.software>
+ * @author    Adeildo Amorim <adeildo@capitaldigitalaberto.com.br>
+ * @copyright 2020 We Dev Tecnologia Ltda
+ * @link      https://github.com/wedevBr/cda-admin-backend-laravel/
+ */
+
+namespace WeDevBr\Bankly\Tests;
+
+use Orchestra\Testbench\TestCase;
+use WeDevBr\Bankly\BankAccount;
+
+
+class BankAccountTest extends TestCase
+{
+    /**
+     * @test
+     */
+    private function validBankAccount()
+    {
+        $bankAccount = new BankAccount();
+        $bankAccount->branch = '0001';
+        $bankAccount->account = '1234552';
+        $bankAccount->document = '01234567890';
+        $bankAccount->name = 'Full name';
+        return $bankAccount;
+    }
+
+    /**
+     * @test
+     */
+    public function testInvalidBankBranch()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectErrorMessage('branch should be a numeric string');
+        $bankAccount = $this->validBankAccount();
+        $bankAccount->branch = null;
+        $bankAccount->validate();
+    }
+
+    /**
+     * @test
+     */
+    public function testInvalidBankAccount()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectErrorMessage('account should be a numeric string');
+
+        $bankAccount = $this->validBankAccount();
+        $bankAccount->account = null;
+        $bankAccount->validate();
+    }
+
+    /**
+     * @test
+     */
+    public function testInvalidBankDocument()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectErrorMessage('document should be a string');
+        $bankAccount = $this->validBankAccount();
+        $bankAccount->document = null;
+        $bankAccount->validate();
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectErrorMessage('cpf_cnpj invalid');
+        $bankAccount->document = '12345678901';
+    }
+
+    /**
+     * @test
+     */
+    public function testInvalidBankName()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectErrorMessage('name should be a string');
+        $bankAccount = $this->validBankAccount();
+        $bankAccount->name = null;
+        $bankAccount->validate();
+    }
+}
