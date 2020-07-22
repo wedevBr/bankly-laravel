25fc7dfc8a0c97edd828c82bf86e1e2bc33fb3e6
Adeildo Júnior <adeildo@capitaldigitalaberto.com.br>
2020-07-22 18:25:13 -0300

    [Fix] Fixed PSR12 on config.php file

--- config/config.php
+++ config/config.php
@@ -2,7 +2,10 @@
 
 return [
     'login_url' => env('BANKLY_LOGIN_URL', 'https://api.acessobank.com.br/baas'), // Default value set to Production
-    'api_url' => env('BANKLY_API_URL', 'https://login.acessobank.com.br/connect/token'), // Defaults value set to Production
+    'api_url' => env(
+        'BANKLY_API_URL',
+        'https://login.acessobank.com.br/connect/token'
+    ), // Defaults value set to Production
     'client_secret' => env('BANKLY_CLIENT_SECRET', null), // Your client secret provided by bankly staff
     'client_id' => env('BANKLY_CLIENT_ID', null) // Your client ID provided by bankly staff
 ];
