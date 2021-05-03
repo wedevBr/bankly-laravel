5b27725d83a772165f84e94941e1c4f9e6ce8275
Rafael Teixeira <rafaeldemeirateixeira@gmail.com>
2021-05-03 17:48:29 -0300

    [Feature] Added PIX cashout transfer request test

---
+++ tests/Pix/PixCashoutTest.php
@@ -0,0 +1,207 @@
+<?php
+
+namespace WeDevBr\Bankly\Tests;
+
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Http;
+use Orchestra\Testbench\TestCase;
+use ReflectionProperty;
+use WeDevBr\Bankly\Bankly;
+use WeDevBr\Bankly\BanklyServiceProvider;
+use WeDevBr\Bankly\Types\Pix\AddressingAccount;
+use WeDevBr\Bankly\Types\Pix\Bank;
+use WeDevBr\Bankly\Types\Pix\BankAccount;
+use WeDevBr\Bankly\Types\Pix\PixCashout;
+
+/**
+ * PixCashoutTest class
+ *
+ * PHP version 7.4|8.0
+ *
+ * @author    WeDev Brasil Team <contato@wedev.software>
+ * @author    Rafael Teixeira <rafaeldemeirateixeira@gmail.com>
+ * @copyright 2020 We Dev Tecnologia Ltda
+ * @link      https://github.com/wedevBr/bankly-laravel
+ */
+class PixCashoutTest extends TestCase
+{
+    use WithFaker;
+
+    protected function getPackageProviders($app)
+    {
+        return [BanklyServiceProvider::class];
+    }
+
+    /**
+     * @return BankAccount
+     */
+    public function validSender()
+    {
+        $addressingAccount = new AddressingAccount();
+        $addressingAccount->branch = '0001';
+        $addressingAccount->number = '1111';
+        $addressingAccount->type = 'CHECKING';
+
+        $bank = new Bank();
+        $bank->ispb = '11112222';
+        $bank->compe = '323';
+        $bank->name = 'Banco Test S.A.';
+
+        $sender = new BankAccount();
+        $sender->documentNumber = '12345678909';
+        $sender->name = 'Jhon Smith';
+        $sender->account = $addressingAccount;
+        $sender->bank = $bank;
+
+        return $sender;
+    }
+
+    /**
+     * @return BankAccount
+     */
+    public function validRecipient()
+    {
+        $addressingAccount = new AddressingAccount();
+        $addressingAccount->branch = '0002';
+        $addressingAccount->number = '2222';
+        $addressingAccount->type = 'CHECKING';
+
+        $bank = new Bank();
+        $bank->ispb = '00000000';
+        $bank->compe = '001';
+        $bank->name = 'Banco BB S.A.';
+
+        $recipient = new BankAccount();
+        $recipient->documentNumber = '12345678909';
+        $recipient->name = 'Sara Smith';
+        $recipient->account = $addressingAccount;
+        $recipient->bank = $bank;
+
+        return $recipient;
+    }
+
+    /**
+     * @return PixCashout
+     */
+    public function validPixCashout()
+    {
+        $pixCashout = new PixCashout();
+        $pixCashout->amount = '83.23';
+        $pixCashout->description = 'Mercado';
+        $pixCashout->sender = $this->validSender();
+        $pixCashout->recipient = $this->validRecipient();
+        $pixCashout->initializationType = 'Key';
+        $pixCashout->addressKey = '123456789';
+        $pixCashout->receiverReconciliationId = 'asdfghjklasdfhjklasdfhjkl';
+        $pixCashout->endToEndId = 'qwertyuiopqwertyuiop';
+
+        return $pixCashout;
+    }
+
+    /**
+     * @return array
+     */
+    public function getFakerHttp(array $response, int $statusCode)
+    {
+        return;
+    }
+
+    public function successResponse()
+    {
+        $response = [
+            'amount' => 99.98,
+            'description' => 'string',
+            'sender' => [
+                'account' => [
+                    'branch' => 'string',
+                    'number' => 'string',
+                    'type' => 'CHECKING',
+                ],
+                'bank' => [
+                    'ispb' => 'string',
+                    'compe' => 'string',
+                    'name' => 'string',
+                ],
+                'documentNumber' => 'string',
+                'name' => 'string',
+            ],
+            'recipient' => [
+                'account' => [
+                    'branch' => 'string',
+                    'number' => 'string',
+                    'type' => 'CHECKING',
+                ],
+                'bank' => [
+                    'ispb' => 'string',
+                    'compe' => 'string',
+                    'name' => 'string',
+                ],
+                'documentNumber' => 'string',
+                'name' => 'string'
+            ],
+            'authenticationCode' => 'string'
+        ];
+
+        Http::fake([
+            config('bankly')['api_url'] . '/pix/cash-out' => Http::response($response, 202)
+        ]);
+    }
+
+    /**
+     * @return void
+     */
+    public function testSuccessPixCashout()
+    {
+        $this->successResponse();
+
+        $client = new Bankly();
+        $token = new ReflectionProperty(Bankly::class, 'token');
+        $token->setAccessible(true);
+        $token->setValue($client, $this->faker->uuid);
+
+        $tokenExpiry = new ReflectionProperty(Bankly::class, 'token_expiry');
+        $tokenExpiry->setAccessible(true);
+        $tokenExpiry->setValue($client, now()->addSeconds(3600)->unix());
+
+        $response = $client->pixCashout($this->validPixCashout(), $this->faker->uuid);
+
+        Http::assertSent(function ($request) {
+            $body = collect($request->data());
+
+            $sender = $body['sender'];
+            $recipient = $body['recipient'];
+
+            return $body['amount'] === '83.23'
+                && $body['description'] === 'Mercado'
+
+                && $sender['account']['branch'] === '0001'
+                && $sender['account']['number'] === '1111'
+                && $sender['account']['type'] === 'CHECKING'
+                && $sender['bank']['ispb'] === '11112222'
+                && $sender['bank']['compe'] === '323'
+                && $sender['bank']['name'] === 'Banco Test S.A.'
+                && $sender['documentNumber'] === '12345678909'
+                && $sender['name'] === 'Jhon Smith'
+
+                && $recipient['account']['branch'] === '0002'
+                && $recipient['account']['number'] === '2222'
+                && $recipient['account']['type'] === 'CHECKING'
+                && $recipient['bank']['ispb'] === '00000000'
+                && $recipient['bank']['compe'] === '001'
+                && $recipient['bank']['name'] === 'Banco BB S.A.'
+                && $recipient['documentNumber'] === '12345678909'
+                && $recipient['name'] === 'Sara Smith'
+
+                && $body['initializationType'] === 'Key'
+                && $body['addressKey'] === '123456789'
+                && $body['receiverReconciliationId'] === 'asdfghjklasdfhjklasdfhjkl'
+                && $body['endToEndId'] === 'qwertyuiopqwertyuiop';
+        });
+
+        $this->assertArrayHasKey('amount', $response);
+        $this->assertArrayHasKey('description', $response);
+        $this->assertArrayHasKey('sender', $response);
+        $this->assertArrayHasKey('recipient', $response);
+        $this->assertArrayHasKey('authenticationCode', $response);
+    }
+}
