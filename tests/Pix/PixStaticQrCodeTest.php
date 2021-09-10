ef995d4dfec2c2f82119c1b203b88fe6da097716
Yan Gonçalves <yanw100@gmail.com>
2021-09-10 00:06:03 -0300

    [Feature] Tests.

---
+++ tests/Pix/PixStaticQrCodeTest.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace WeDevBr\Bankly\Tests;
+
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Http;
+use WeDevBr\Bankly\BanklyServiceProvider;
+use WeDevBr\Bankly\Types\Card\Address;
+use WeDevBr\Bankly\Types\Card\Duplicate;
+use WeDevBr\Bankly\BanklyCard;
+use WeDevBr\Bankly\Types\Pix\AddressingKey;
+use WeDevBr\Bankly\BanklyPix;
+use WeDevBr\Bankly\Types\Pix\PixStaticQrCode;
+
+/**
+ * DuplicateCardTest class
+ *
+ * PHP version 7.4|8.0
+ *
+ * @author    WeDev Brasil Team <contato@wedev.software>
+ * @author    Yan de Paula <yanw100@gmail@gmail.com>
+ * @copyright 2021 We Dev Tecnologia Ltda
+ * @link      https://github.com/wedevBr/bankly-laravel
+ */
+class PixStaticQrCodeTest extends TestCase
+{
+    use WithFaker;
+
+    /**
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->auth();
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [BanklyServiceProvider::class];
+    }
+
+    /**
+     * @return Address
+     */
+    private function validAddressingKey()
+    {
+        $addressingKey = new AddressingKey();
+        $addressingKey->type = 'CPF';
+        $addressingKey->value = '00000000000';
+
+        return $addressingKey;
+    }
+
+    /**
+     * @return PixStaticQrCode
+     */
+    private function validStaticQrCodeData()
+    {
+        $duplicateCard = new PixStaticQrCode();
+        $duplicateCard->recipientName = 'Place Holder';
+        $duplicateCard->addressingKey = $this->validAddressingKey();
+
+        return $duplicateCard;
+    }
+
+    /**
+     * @return array
+     */
+    public function getFakerHttp(string $path, array $response, int $statusCode = 200)
+    {
+        return [
+            config('bankly')['api_url'] . "{$path}" => Http::response($response, $statusCode)
+        ];
+    }
+
+    /**
+     * @return void
+     */
+    public function testSuccessStaticQrCode()
+    {
+        $encoded = "MDAwMjAxMjYzMzAwMTRici5nb3YuYmNiLnBpeDAxMTE1NjUyNzkzODIxNzUyMDQwMDAwNTMwMzk4NjU0MDQxLjAwN";
+
+        Http::fake($this->getFakerHttp("/baas/pix/qrcodes", [
+            'encodedValue' => $encoded,
+        ], 200));
+
+        $card = new BanklyPix();
+        $response = $card->qrCode($this->validStaticQrCodeData());
+
+        Http::assertSent(function ($request) {
+            $body = collect($request->data());
+            $addressingKey = $body['addressingKey'];
+
+            return $body['recipientName'] === 'Place Holder'
+                && $addressingKey['type'] === 'CPF'
+                && $addressingKey['value'] === '00000000000';
+        });
+
+        $this->assertArrayHasKey('encodedValue', $response);
+    }
+
+    /**
+     * @return void
+     */
+    public function testValidateIfAddressingKeyTypeIsValid()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectErrorMessage('this key type is not valid');
+        $pixEntries = $this->validStaticQrCodeData();
+        $pixEntries->addressingKey->type = 'RG';
+        $pixEntries->validate();
+    }
+
+    /**
+     * @return void
+     */
+    public function testValidateIfAddressingKeyTypeIsString()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectErrorMessage('type should be a string');
+        $pixEntries = $this->validStaticQrCodeData();
+        $pixEntries->addressingKey->type = null;
+        $pixEntries->validate();
+    }
+
+    /**
+     * @return void
+     */
+    public function testValidateIfRecipientNameIsInvalid()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectErrorMessage('recipient name should be a string');
+        $pixEntries = $this->validStaticQrCodeData();
+        $pixEntries->recipientName = null;
+        $pixEntries->validate();
+    }
+}
